8503bd7808ea6e557071950555b5b0ea74157f6f
ola wozniak <olawozniak551@gmail.com>
2022-03-06 13:11:32 +0100

    task 16.1

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -1,6 +1,10 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Icon = props => (<i className={`fas fa-${props.name}`}></i>);
 
+Icon.propTypes = {
+  name: PropTypes.string.isRequired,
+};
 export default Icon;
